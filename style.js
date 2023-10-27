3016edc7728f02fc007d6edda7178cfdaf271cb2
hellosamm <142949327+hellosamm@users.noreply.github.com>
2023-10-27 08:10:35 -1000

    grid added

---
+++ style.js
@@ -0,0 +1,12 @@
+let board = document.querySelector(".board");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
+  }
+}
+
+const eachCell = document.querySelectorAll(".cell");
+eachCell.addEventListener("hover", function () {});
