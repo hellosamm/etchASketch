1ee46c9f9540f33ba2c39b36bef50e946eeb6923
hellosamm <142949327+hellosamm@users.noreply.github.com>
2023-10-31 07:56:55 -1000

    slider working,changing colors and reset button not working if size is bigger than 16

--- style.js
+++ style.js
@@ -8,10 +8,10 @@ for (let i = 0; i < 16; i++) {
     board.appendChild(cell);
   }
 }
-
-// creating hover state so grid cells change to black when you hover over them
+//define individualCells for use in functions
 const individualCells = document.querySelectorAll(".cell");
 
+// creating hover state so grid cells change to black when you hover over them
 individualCells.forEach((cell) => {
   cell.addEventListener("mouseenter", (e) => {
     cell.style.backgroundColor = "#000";
@@ -29,14 +29,77 @@ function clearTheBoard() {
 const restartBtn = document.getElementById("restart");
 restartBtn.addEventListener("click", clearTheBoard);
 
-// //change color function
-// function changeColor() {
-//   cell.style.backgroundColor = "#00FF";
-// }
+// array of colors to cycle through
+const colors = [
+  "#00FF",
+  "#ff0",
+  "#008000",
+  "#ff0000",
+  "#ffc0cb",
+  "#fff",
+  "#000",
+];
+let colorIndex = 6;
 
-// // button that changes the color of the hover state
-// const colorBtn = document.getElementById("changeColor");
-// colorBtn.addEventListener("click", changeColor);
+//change color function
+function changeColor() {
+  individualCells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
+      cell.style.backgroundColor = colors[colorIndex];
+    });
+  });
 
-// //update board size function
-// function updateBoardSize() {}
+  colorIndex = (colorIndex + 1) % colors.length;
+}
+
+// chance the color of the circle
+let colorCircle = document.getElementById("circle");
+function changeCircleColor() {
+  circle.style.backgroundColor = colors[colorIndex];
+}
+
+// button that changes the color of the hover state/ "pen"
+const colorBtn = document.getElementById("changeColor");
+colorBtn.addEventListener("click", function () {
+  changeColor();
+  changeCircleColor();
+});
+
+//update board size function
+function updateBoardSize(size) {
+  board.innerHTML = "";
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      board.appendChild(cell);
+    }
+  }
+
+  //shrink the cell to still fit inside the board
+  const newSize = 100 / size + "%";
+  const individualCells = document.querySelectorAll(".cell");
+  individualCells.forEach((cell) => {
+    cell.style.width = newSize;
+    cell.style.height = newSize;
+  });
+
+  individualCells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
+      cell.style.backgroundColor = "#000";
+    });
+  });
+
+  changeColor();
+}
+
+//make the slider responsive
+const slider = document.getElementById("slider");
+const sliderText = document.getElementById("sliderText");
+
+slider.addEventListener("input", function () {
+  const size = parseInt(this.value);
+  sliderText.textContent = size;
+  updateBoardSize(size);
+});
